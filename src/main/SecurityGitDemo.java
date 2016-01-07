9c8b1f3d30358b02c91cf3dec6278d7aa2674252
Siva Kumar <kumar.hsiva@tcs.com>
2016-01-07 19:22:39 +0530

    Created Draft version

---
+++ src/main/SecurityGitDemo.java
@@ -0,0 +1,16 @@
+package main;
+
+public class SecurityGitDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a = 10, b = 20;
+		System.out.println("sum of a and b is" + add(a, b));
+	}
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+}
